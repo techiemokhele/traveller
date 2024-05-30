7149713cca81b3981bbaee31e0a5a806f2d4cc11
techiemokhele <neomokhele23@gmail.com>
2024-05-30 10:54:23 +0200

    Implement guide ui setup, update hero and nav ui

--- components/section/HeroComponent.tsx
+++ components/section/HeroComponent.tsx
@@ -3,7 +3,7 @@ import ButtonComponent from "../common/ButtonComponent";
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-16 xl:flex-row">
       <div className="hero-map" />
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
@@ -12,7 +12,7 @@ const Hero = () => {
           alt="camp"
           width={50}
           height={50}
-          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+          className="absolute left-[-5px] top-[-28px] lg:top-[-34px] w-10 lg:w-[50px]"
         />
         <h1 className="bold-52 lg:bold-64">Mokhele Travel Camp Area</h1>
         <p className="regular-16 text-gray-30 mt-6 xl:max-w-[520px]">
@@ -60,24 +60,24 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="relative flex flex-1 items-start">
+      <div className="relative flex flex-1 items-start lg:mt-6">
         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Mountain Bush Lake</p>
           </div>
 
           <div className="flexBetween">
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Distance</p>
-              <p className="bold-20 text-white">173.28 mi</p>
+              <p className="bold-20 text-white">54.28 km</p>
             </div>
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Elevation</p>
-              <p className="bold-20 text-white">2.040 km</p>
+              <p className="bold-20 text-white">2.5 km</p>
             </div>
           </div>
         </div>
